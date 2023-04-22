060114721171e8ba6b3d0ef4cb2cf227f10018bf
Zachary Daniels <zjdaniels1985@live.com>
2023-04-22 13:40:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-docker rmi zjdaniels1985/activemq
-docker build -t zjdaniels1985/activemq:latest .
+docker rmi zjdaniels1985/activemq 2>/dev/null \ 
+docker build . -t zjdaniels1985/activemq:latest && \
+docker push zjdaniels1985/activemq:latest
